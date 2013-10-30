de3e61c5829f7a117d2b814057a2cf2a5f307b5e
TheJeterLP <joey.peter1998@gmail.com>
2013-10-30 17:19:39 +0100

    [FIX] Added SearchForUpdates to config

--- src/main/java/de/JeterLP/MakeYourOwnCommands/utils/ConfigFile.java
+++ src/main/java/de/JeterLP/MakeYourOwnCommands/utils/ConfigFile.java
@@ -18,26 +18,30 @@ public class ConfigFile {
     }
 
     /**
-     * <p>This method loads the config of this plugin</p>
+     * <p>
+     * This method loads the config of this plugin</p>
      */
-    public boolean loadConfig() {
-        File cfg = new File("plugins/MakeYourOwnCommands/config.yml");
+    public void loadConfig() {
+        File cfg = new File("plugins" + File.separator + "MakeYourOwnCommands" + File.separator + "config.yml");
         main.getDataFolder().mkdirs();
         YamlConfiguration config = YamlConfiguration.loadConfiguration(cfg);
         if (cfg.exists()) {
             if (config.getInt("Version") != 1) {
-                return false;
+                 cfg.renameTo(new File("plugins" + File.separator + "MakeYourOwnCommands" + File.separator + "config_old.yml"));
+                 generate();
             }
             main.getConfig().options().copyDefaults(true);
-            return true;
         } else {
-            main.saveDefaultConfig();
+            generate();
             main.getConfig().options().copyDefaults(true);
-            return true;
         }
     }
 
     public FileConfiguration getConfig() {
         return main.getConfig();
+    }
+
+    private void generate() {
+        main.saveResource("config.yml", false);
     }
 }
