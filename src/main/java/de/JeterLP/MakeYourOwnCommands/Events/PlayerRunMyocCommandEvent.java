40fa1c821188375d9d620e0d09a24b18e67b9aff
joey.peter1998@gmail.com <joey.peter1998@gmail.com>
2014-05-01 15:25:35 +0200

    Formatting

--- src/main/java/de/JeterLP/MakeYourOwnCommands/Events/PlayerRunMyocCommandEvent.java
+++ src/main/java/de/JeterLP/MakeYourOwnCommands/Events/PlayerRunMyocCommandEvent.java
@@ -12,70 +12,70 @@ import org.bukkit.event.player.PlayerEvent;
  */
 public class PlayerRunMyocCommandEvent extends PlayerEvent implements Cancellable {
 
-        private boolean cancelled = false;
-        private final Command cmd;
-        private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled = false;
+    private final Command cmd;
+    private static final HandlerList handlers = new HandlerList();
 
-        public PlayerRunMyocCommandEvent(Player who, Command cmd) {
-                super(who);
-                this.cmd = cmd;
-        }
+    public PlayerRunMyocCommandEvent(Player who, Command cmd) {
+        super(who);
+        this.cmd = cmd;
+    }
 
-        /**
-         * Bukkit Method.
-         *
-         * @return handlers
-         */
-        @Override
-        public HandlerList getHandlers() {
-                return handlers;
-        }
+    /**
+     * Bukkit Method.
+     *
+     * @return handlers
+     */
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
-        /**
-         * Bukkit Method.
-         *
-         * @return handlers
-         */
-        public static HandlerList getHandlerList() {
-                return handlers;
-        }
+    /**
+     * Bukkit Method.
+     *
+     * @return handlers
+     */
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
-        /**
-         * Gets the executed {@link de.JeterLP.MakeYourOwnCommands.Command.Command}.
-         *
-         * @return cmd
-         */
-        public Command getCommand() {
-                return cmd;
-        }
+    /**
+     * Gets the executed {@link de.JeterLP.MakeYourOwnCommands.Command.Command}.
+     *
+     * @return cmd
+     */
+    public Command getCommand() {
+        return cmd;
+    }
 
-        /**
-         * gets the {@link de.JeterLP.MakeYourOwnCommands.Command.CommandType} of the executed command.
-         *
-         * @return CommandType
-         */
-        public CommandType getType() {
-                return cmd.getType();
-        }
+    /**
+     * gets the {@link de.JeterLP.MakeYourOwnCommands.Command.CommandType} of the executed command.
+     *
+     * @return CommandType
+     */
+    public CommandType getType() {
+        return cmd.getType();
+    }
 
-        /**
-         * Checks if the event was cancelled.
-         *
-         * @return cancelled
-         */
-        @Override
-        public boolean isCancelled() {
-                return cancelled;
-        }
+    /**
+     * Checks if the event was cancelled.
+     *
+     * @return cancelled
+     */
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
 
-        /**
-         * Changes the status of the event. If bln is true, the event will not be executed.
-         *
-         * @param bln: Sets cancelled to bln
-         */
-        @Override
-        public void setCancelled(boolean bln) {
-                cancelled = bln;
-        }
+    /**
+     * Changes the status of the event. If bln is true, the event will not be executed.
+     *
+     * @param bln: Sets cancelled to bln
+     */
+    @Override
+    public void setCancelled(boolean bln) {
+        cancelled = bln;
+    }
 
 }
